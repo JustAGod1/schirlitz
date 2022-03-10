e672ae6a4fd063373c13a397cf57882aff4f34c3
JustAGod <hvalick2111@gmail.com>
2022-03-10 18:14:21 +0300

    added random and auto update

--- src/bot.rs
+++ src/bot.rs
@@ -1,7 +1,10 @@
 use std::collections::{HashMap, HashSet};
+use std::process::Command;
 
 use futures::StreamExt;
-use telegram_bot::{Api, CanAnswerInlineQuery, InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent, InputTextMessageContent, Message, MessageChat, MessageText, SendMessage, UpdateKind};
+use rand::Rng;
+use telegram_bot::{Api, CanAnswerInlineQuery, InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent, InputTextMessageContent, Message, MessageChat, MessageText, SendMessage, UpdateKind, User};
+use crate::{Chat, UserId};
 
 use crate::database::DatabaseAccessor;
 
@@ -56,6 +59,54 @@ impl SchirlitzBot {
             self.waiting_from.insert(msg.chat.id().to_string(), Self::joke_sent);
             self.api.spawn(req);
         }
+        if text.starts_with("/restart") {
+            if msg.from.id != UserId(429171352) {
+                self.api.spawn(SendMessage::new(chat, "Иди нахуй бесправный мудила"));
+                return;
+            }
+            self.update(msg.chat);
+        }
+    }
+
+    fn update(&mut self, chat: MessageChat) {
+        self.api.spawn(SendMessage::new(chat.clone(), "Пулю"));
+
+        let mut command = Command::new("git")
+            .arg("pull");
+
+        if !self.run_command(command, &chat) { return; }
+        self.api.spawn(SendMessage::new(chat.clone(), "Перезапускаюсь"));
+
+        let mut command = Command::new("sudo")
+            .arg("systemctl")
+            .arg("restart")
+            .arg("bot");
+
+        self.run_command(command, &chat);
+    }
+
+    fn run_command(&mut self, cmd: &mut Command, chat: &MessageChat) -> bool {
+        return match cmd.output() {
+            Ok(output) => {
+                if output.status != 0 {
+                    self.api.spawn(SendMessage::new(
+                        chat,
+                        format!(
+                            "Процесс завершился с кодом {}:\n {}, {}",
+                            output.status,
+                            String::from_utf8_lossy(&output.stderr),
+                            String::from_utf8_lossy(&output.stdout)
+                        ),
+                    ));
+                    return false;
+                }
+                true
+            }
+            Err(err) => {
+                self.api.spawn(SendMessage::new(chat, format!("Произошла ошибка: {}", err)));
+                false
+            }
+        }
     }
 
     fn joke_sent(&mut self, msg: Message) {
@@ -66,31 +117,46 @@ impl SchirlitzBot {
         self.api.spawn(req);
     }
 
-    fn process_inline(&mut self, query: InlineQuery) {
-        let jokes = self.database.query_jokes(&query.query);
-        let answers: Vec<InlineQueryResult> = jokes.iter().enumerate().map(|(i, joke)| {
-            InlineQueryResult::InlineQueryResultArticle(
-                InlineQueryResultArticle {
-                    id: i.to_string(),
-                    title: joke.text.to_string(),
-                    input_message_content: InputMessageContent::InputTextMessageContent(
-                        InputTextMessageContent {
-                            message_text: joke.text.to_string(),
-                            parse_mode: None,
-                            disable_web_page_preview: true,
-                        }
-                    ),
-                    reply_markup: None,
-                    url: None,
-                    hide_url: false,
-                    description: None,
-                    thumb_url: None,
-                    thumb_width: None,
-                    thumb_height: None,
-                }
-            )
-        }).collect();
+    fn make_joke_answer(id: String, title: String, joke: String) -> InlineQueryResult {
+        InlineQueryResult::InlineQueryResultArticle(
+            InlineQueryResultArticle {
+                id,
+                title: joke.clone(),
+                input_message_content: InputMessageContent::InputTextMessageContent(
+                    InputTextMessageContent {
+                        message_text: joke,
+                        parse_mode: None,
+                        disable_web_page_preview: true,
+                    }
+                ),
+                reply_markup: None,
+                url: None,
+                hide_url: false,
+                description: None,
+                thumb_url: None,
+                thumb_width: None,
+                thumb_height: None,
+            }
+        )
+    }
 
-        self.api.spawn(query.answer(answers))
+    fn process_inline(&mut self, query: InlineQuery) {
+        let jokes = self.database.query_jokes(query.query.as_str());
+        let answers: Vec<InlineQueryResult>;
+        if query.query.is_empty() {
+            answers = vec![Self::make_joke_answer(
+                "rand".to_string(),
+                "Случайно".to_string(),
+                jokes.get(rand::thread_rng().gen_range(0..jokes.len())).unwrap().text.clone()
+            )]
+        } else {
+            answers = jokes.iter().enumerate().map(|(i, joke)| {
+                Self::make_joke_answer(i.to_string(), joke.text.clone(), joke.text.clone());
+            }).collect();
+        }
+
+        let mut answer = query.answer(answers);
+        answer.cache_time(0);
+        self.api.spawn(answer)
     }
 }
